31d56f0d4f6885a4d50fecdb49be2911b4c483a5
RodrigoOliveira <rodrigooliveira.cont17@gmail.com>
2025-08-25 18:25:24 -0300

    fix: delete const

--- src/controllers/task/ListTasksController.ts
+++ src/controllers/task/ListTasksController.ts
@@ -4,7 +4,6 @@ import { ListTasksService } from "../../services/task/ListTasksService";
 class ListTasksController {
     async handle(req: Request, res: Response){
         const listTasksService = new ListTasksService
-        const { name } = req.body
         const user_id = req.user_id;
 
         const tasks = await listTasksService.execute(user_id);
